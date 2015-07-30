9dd1066e614f5407ba970dbe69b48bb5e16cd372
John McEnery <jmcenery@manifestcorp.com>
2015-07-30 00:30:58 -0400

    added todo to fixajax comments refresh issue

--- grails-blog/grails-app/controllers/grails/blog/CommentController.groovy
+++ grails-blog/grails-app/controllers/grails/blog/CommentController.groovy
@@ -4,6 +4,8 @@ package grails.blog
 class CommentController {
 
     def addComment() {
+    	//TODO: Should Save Post instead of comment
+    	//add comment to post before save
     	Postpage post = Postpage.get(params.postId)
     	def comment = new Comment(post:post, 
     							  author:params.author, 
